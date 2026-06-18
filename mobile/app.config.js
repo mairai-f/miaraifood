a08902d4db01e39eee412ca1b81cb98ace7ee97f
celioantonio7 <celioantonio.dev@gmail.com>
2026-06-18 17:20:36 -0300

    chore: atualiza versao mobile

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -7,7 +7,7 @@ module.exports = {
   expo: {
     name: isFoodContext ? 'HappyCashFood Mobile' : 'HappyCash Mobile',
     slug: isFoodContext ? 'happycashfood-mobile' : 'happycash-mobile',
-    version: '0.1.35',
+    version: '0.1.36',
     orientation: 'portrait',
     scheme: isFoodContext ? 'happycashfood' : 'happycash',
     userInterfaceStyle: 'dark',
