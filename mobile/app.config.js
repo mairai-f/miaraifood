7aeac6314056178a820da3f8d9539eacd5ecdf24
celioantonio7 <celioantonio.dev@gmail.com>
2026-07-14 20:50:05 -0300

    Bump release para 0.1.56

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -7,7 +7,7 @@ module.exports = {
   expo: {
     name: isFoodContext ? 'HappyCashFood Mobile' : 'HappyCash Mobile',
     slug: isFoodContext ? 'happycashfood-mobile' : 'happycash-mobile',
-    version: '0.1.55',
+    version: '0.1.56',
     orientation: 'portrait',
     scheme: isFoodContext ? 'happycashfood' : 'happycash',
     userInterfaceStyle: 'dark',
