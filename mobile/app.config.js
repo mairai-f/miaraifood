55a7cd0ad4e15e3b96ab40ef104c06f1d2b123e1
celioantonio7 <celioantonio.dev@gmail.com>
2026-07-15 19:41:40 -0300

    Prepara release 0.1.59

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -7,7 +7,7 @@ module.exports = {
   expo: {
     name: isFoodContext ? 'HappyCashFood Mobile' : 'HappyCash Mobile',
     slug: isFoodContext ? 'happycashfood-mobile' : 'happycash-mobile',
-    version: '0.1.58',
+    version: '0.1.59',
     orientation: 'portrait',
     scheme: isFoodContext ? 'happycashfood' : 'happycash',
     userInterfaceStyle: 'dark',
